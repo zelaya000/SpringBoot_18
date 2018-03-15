77458ff290729f8a62e014ae24d6eaa455075650
zelaya <victorzelaya9462@gmail.com>
2018-03-14 20:55:15 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot18Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot18Application.class, args);
+	}
+}
